c23c5e397326110def1a110bd2b4e624ec4ded73
Enes Demir <enesdemr@outlook.com.tr>
2022-06-24 14:14:45 +0300

    update

--- app/src/main/java/com/example/jettipcompose/MainActivity.kt
+++ app/src/main/java/com/example/jettipcompose/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.jettipcompose
 
+
 import android.os.Bundle
+import android.transition.Slide
+import android.util.Log
+import android.widget.Space
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
@@ -8,83 +12,172 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Remove
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.BlendMode.Companion.Color
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.example.jettipcompose.components.InputField
 import com.example.jettipcompose.ui.theme.JetTipComposeTheme
+import com.example.jettipcompose.widgets.RoundIconButton
+
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalComposeUiApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetTipComposeTheme {
-                MyApp {
-                    TopHeader()
-                }
+            MyApp {
+                //   TopHeader()
+                MainContent()
             }
         }
     }
 }
+
 @Composable
 fun MyApp(content: @Composable () -> Unit){
-    Surface(
-        modifier = Modifier.fillMaxSize(),
-        color = MaterialTheme.colors.background
-    ) {
+    JetTipComposeTheme {
+        Surface(color = MaterialTheme.colors.background) {
+            content()
+        }
     }
 }
-
 @Preview
 @Composable
-fun TopHeader(totalPerPerson: Int = 142){
+fun TopHeader(totalPerPerson : Double = 0.0){
     Surface(modifier = Modifier
         .fillMaxWidth()
         .height(150.dp)
         .clip(shape = CircleShape.copy(all = CornerSize(12.dp))),
-        color = Color(0xFFE9D7F7)
+        color = Color(0xFFE9D7D7)
     ) {
         Column(
-            modifier = Modifier
-                .padding(12.dp),
+            modifier = Modifier.padding(12.dp),
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center
         ) {
             val total = "%.2f".format(totalPerPerson)
-            Text(text = "Total Per Person",
+            Text(text ="Total Per Person",
                 style = MaterialTheme.typography.h5
             )
-            Text(text = "$$total",
+            Text(text = "$$totalPerPerson",
                 style = MaterialTheme.typography.h4,
                 fontWeight = FontWeight.ExtraBold
             )
-
         }
 
     }
 }
+
+
 @Preview
 @Composable
-fun MainContent() {
+fun MainContent(){
+    BillForm(){
+            billAmt ->
+        run {
+            Log.d("AMT", "MainContent ${billAmt.toInt() * 100}")
+        }
+    }
+
+}
+
+
+@OptIn(ExperimentalComposeUiApi::class)
+@Composable
+fun BillForm(modifier: Modifier = Modifier,
+             onValChange: (String) -> Unit = {}
+){
+    val totalBillState = remember{
+        mutableStateOf("")
+    }
+    val validState = remember(totalBillState.value) {
+        totalBillState.value.trim().isNotEmpty()
+    }
+    val sliderPositionState = remember {
+        mutableStateOf(0f)
+    }
+    val keyboardController = LocalSoftwareKeyboardController.current
     Surface(
         modifier = Modifier
             .padding(2.dp)
             .fillMaxWidth(),
-                shape = RoundedCornerShape(corner = CornerSize(8.dp)),
-                border = BorderStroke(width = 1.dp, color = androidx.compose.ui.graphics.Color.LightGray)){
-        Column() {
-            Text("Hello again...")
-            Text("Hello again...")
-            Text("Hello again...")
+        shape = RoundedCornerShape(corner = CornerSize(8.dp)),
+        border = BorderStroke(width = 1.dp, color = Color.LightGray)
+    ) {
+        Column(
+            modifier = Modifier.padding(6.dp),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.Start
+        ) {
 
+            InputField(
+                valueState = totalBillState,
+                labelId = "Enter Bill",
+                enabled = true,
+                isSingleLinge = true,
+                onAction = KeyboardActions{
+                    if(!validState) return@KeyboardActions
+                    onValChange(totalBillState.value.trim())
+                    keyboardController?.hide()
+                })
+            if(validState){
+                Row(modifier = Modifier.padding(3.dp),
+                    horizontalArrangement = Arrangement.Start) {
+                    Text(text = "Split",
+                    modifier = Modifier.align(
+                        alignment = Alignment.CenterVertically))
+                    Spacer(modifier = Modifier.width(120.dp))
+                    Row(
+                        modifier = Modifier.padding(horizontal = 3.dp),
+                        horizontalArrangement = Arrangement.End) {
+                        RoundIconButton(
+                            imageVector = Icons.Default.Remove,
+                            onClick = { })
+                        Text(text ="2",
+                            modifier = Modifier
+                                .align(Alignment.CenterVertically)
+                                .padding(start = 9.dp, end = 9.dp))
+                        RoundIconButton(
+                            imageVector = Icons.Default.Add,
+                            onClick = { })
+                    }
+
+                }
+                Row(modifier = Modifier.padding(horizontal = 3.dp, vertical = 12.dp)){
+                    Text(text = "Text",
+                    modifier = Modifier.align(alignment = Alignment.CenterVertically))
+                    Spacer(modifier = Modifier.width(200.dp))
+                    Text(text = "$33.00", modifier = Modifier.align(alignment = Alignment.CenterVertically))
+                }
+                Column(verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
+                    Text(text = "%33")
+                    Spacer(modifier = Modifier.height(14.dp))
+                    Slider(value = sliderPositionState.value, onValueChange = {newVal ->
+                        sliderPositionState.value = newVal
+                        Log.d("slider","$newVal")
+                    })
+                }
+            }else{
+                Box() {
+
+                }
+            }
         }
 
     }
@@ -95,6 +188,8 @@ fun MainContent() {
 @Composable
 fun DefaultPreview() {
     JetTipComposeTheme {
-        Text(text = "Hello Again")
+        MyApp {
+            Text(text = "Hello Again")
+        }
     }
 }
